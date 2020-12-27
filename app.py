d5308a5d4668878e6fa9e1328f2096b1d6d1fed2
EKO YUDHI PRASTOWO <060116102@intranet.pajak.go.id>
2020-12-27 11:20:55 +0700

    edited

--- app.py
+++ app.py
@@ -9,34 +9,34 @@ from PIL import Image
 import warnings
 warnings.simplefilter('error',Image.DecompressionBombWarning)
 
-app = Flask(__name__, static_folder='imgs')
+app = Flask(__name__)
 #bootstrap = Bootstrap(app)
 
 @app.route('/')
 def do_get():
     return render_template('index.html')
 
-@app.route('/saveimage', methods=['POST'])
-def saveImage():
-    event = request.form.to_dict()
+# @app.route('/saveimage', methods=['POST'])
+# def saveImage():
+#     event = request.form.to_dict()
 
-    dir_name = 'imgs'
-    img_name = uuid.uuid4().hex
+#     dir_name = 'imgs'
+#     img_name = uuid.uuid4().hex
 
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-    with open(os.path.join(dir_name, '{}.jpg'.format(img_name)),'wb') as img:
-        img.write(base64.b64decode(event['image'].split(",")[1]))
+#     if not os.path.exists(dir_name):
+#         os.makedirs(dir_name)
+#     with open(os.path.join(dir_name, '{}.jpg'.format(img_name)),'wb') as img:
+#         img.write(base64.b64decode(event['image'].split(",")[1]))
     
-    original = Image.open(os.path.join(dir_name, '{}.jpg'.format(img_name)))
+#     original = Image.open(os.path.join(dir_name, '{}.jpg'.format(img_name)))
 
-    if(original.format != 'JPEG'):
-        return make_response('Unsupported image type.', 400)
+#     if(original.format != 'JPEG'):
+#         return make_response('Unsupported image type.', 400)
     
-    original.thumbnail((240,240), Image.ANTIALIAS)
-    original.save(os.path.join(dir_name, '{}_240.jpg'.format(img_name)),'JPEG')
+#     original.thumbnail((240,240), Image.ANTIALIAS)
+#     original.save(os.path.join(dir_name, '{}_240.jpg'.format(img_name)),'JPEG')
 
-    return make_response(img_name, 200)
+#     return make_response(img_name, 200)
 
 if __name__ == '__main__':
     app.debug = True
