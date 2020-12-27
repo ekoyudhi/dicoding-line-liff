5a407d62f7bf682f097f4ecf23376eed64515bc5
EKO YUDHI PRASTOWO <060116102@intranet.pajak.go.id>
2020-12-27 11:28:53 +0700

    edited

--- app.py
+++ app.py
@@ -39,5 +39,10 @@ def do_get():
 #     return make_response(img_name, 200)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+    app.run(debug=options.debug, port=options.port)
